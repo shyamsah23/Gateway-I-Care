3e023f5452e13262b2325fd775d852ca7bb1ff10
shyamsah23 <92447257+shyamsah23@users.noreply.github.com>
2025-10-18 22:43:26 +0530

    gateway Repo

---
+++ src/main/java/com/example/Gateway_I_Care/GatewayICareApplication.java
@@ -0,0 +1,13 @@
+package com.example.Gateway_I_Care;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayICareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayICareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Gateway_I_Care/GatewayICareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Gateway_I_Care;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayICareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
