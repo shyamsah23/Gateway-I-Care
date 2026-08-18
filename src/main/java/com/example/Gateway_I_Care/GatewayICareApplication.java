16b8a45175c81e8195e464e32baf914a9d5547bb
shyamsah23 <92447257+shyamsah23@users.noreply.github.com>
2026-08-18 23:14:47 +0530

    Commit-2

--- src/main/java/com/example/Gateway_I_Care/GatewayICareApplication.java
+++ src/main/java/com/example/Gateway_I_Care/GatewayICareApplication.java
@@ -8,7 +8,7 @@ public class GatewayICareApplication {
 
 	public static void main(String[] args) {
 		System.out.println("Commit-1");
-		
+		System.out.println("Commit-2");
 		SpringApplication.run(GatewayICareApplication.class, args);
 	}
 
