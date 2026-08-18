4618467a9afc894aa94cb140aa92651f7817a758
shyamsah23 <92447257+shyamsah23@users.noreply.github.com>
2026-08-18 23:12:51 +0530

    Commit-1

--- src/main/java/com/example/Gateway_I_Care/GatewayICareApplication.java
+++ src/main/java/com/example/Gateway_I_Care/GatewayICareApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GatewayICareApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Commit-1");
+		
 		SpringApplication.run(GatewayICareApplication.class, args);
 	}
 
